618854cbaaef29be8b3d6790f015d65e3c98388d
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2023-12-13 18:56:30 -0300

    feat(storage): adding Storage class to manipulate chrome.storage.local

---
+++ scripts/storage.js
@@ -0,0 +1,22 @@
+class Storage {
+  constructor() {
+    console.log('Storage constructor');
+    this.chromeStorage = chrome.storage.local;
+  }
+
+  async get(key) {
+    const result = await this.chromeStorage.get([key]);
+    return result[key];
+  }
+
+  async set(key, value) {
+    return this.chromeStorage.set({ [key]: value });
+  }
+
+  async remove(key) {
+    return this.chromeStorage.remove(key);
+  }
+}
+
+const storage = new Storage();
+export default storage;
