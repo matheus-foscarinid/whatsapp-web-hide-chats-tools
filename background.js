63cdd94fadd58f8f305a49193bb4d261ec9936b2
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2023-12-19 20:36:48 -0300

    feat(background): Add extension state initialization and icon update
    This commit adds functionality to initialize the extension state and update the icon based on the extension state. It also adds a listener for extension state changes.

--- background.js
+++ background.js
@@ -1,22 +1,42 @@
+import storage from './scripts/storage.js';
+
 const DEFAULT_ICON = 'images/icon/icon-16.png';
 const DISABLED_ICON = 'images/icon/icon-16-disabled.png';
 
-const setIconAccordingToExtensionState = (state) => {
-  const iconPath = state ? DEFAULT_ICON : DISABLED_ICON;
-  chrome.action.setIcon({ path: iconPath });
-}
-
-const addExtensionStateListener = () => {
-  chrome.storage.onChanged.addListener((changes) => {
-    const { currentState } = changes;
-    setIconAccordingToExtensionState(currentState.newValue);
-  });
+// with this, we can add new configs to the extension without breaking the old ones
+// and it will be automatically added to the storage when the user updates the extension
+const DEFAULT_STATE = {
+  currentState: false,
+  blurIntensity: 10,
+  pictureBlur: true,
+  messageBlur: true,
+  contactNameBlur: true,
+  disableClicks: false,
 }
 
 const initializeExtensionState = async () => {
-  const { currentState } = await chrome.storage.local.get(['currentState']);
-  setIconAccordingToExtensionState(currentState);
+  const keys = Object.keys(DEFAULT_STATE);
+  const storedValues = await storage.get(keys);
+
+  keys.forEach((key) => {
+    const currentValue = storedValues[key];
+    currentValue === undefined && storage.set(key, DEFAULT_STATE[key]);
+  });
+
   addExtensionStateListener();
 }
 
-initializeExtensionState();
+const addExtensionStateListener = () => {
+  chrome.storage.onChanged.addListener(setIconAccordingToExtensionState);
+}
+
+const setIconAccordingToExtensionState = async () => {
+  const currentState = await storage.get('currentState')
+  const iconPath = currentState ? DEFAULT_ICON : DISABLED_ICON;
+  chrome.action.setIcon({ path: iconPath });
+}
+
+chrome.runtime.onStartup.addListener(() => {
+  initializeExtensionState();
+  setIconAccordingToExtensionState();
+})
